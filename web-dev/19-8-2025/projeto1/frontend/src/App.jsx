e21031723befa7bf2a51a333d1de1b3dfe30621d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-19 09:48:41 -0300

    Aprensentando react e vite (Web-Dev)

---
+++ web-dev/19-8-2025/projeto1/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+    // DECLARACAO DE VARIAVEIS
+  let nome = "Fiap";
+
+  return (
+   
+
+    <>
+      <h1>Teste</h1>
+      <p>{nome}</p>
+    </>
+  )
+}
+
+export default App
